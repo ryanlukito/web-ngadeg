ca5f95ffd271f41030801038136a34fe01e81be4
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-05-01 12:19:40 +0700

    feat: recent routing method

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import Header from './Header';
-import Nav from './Nav';
-import Footer from './Footer';
-import Home from './Home';
-import NewPost from './NewPost';
-import PostPage from './PostPage';
-import About from './About';
-import Missing from './Missing';
-import {BrowserRouter, Route, Routes, useNavigate}from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { Outlet, useOutlet } from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path='/header' element={<Header/>}/>
-          <Route path='/nav' element={<Nav/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/post' element={<NewPost/>}/>
-          <Route path='/post/:id' element={<PostPage/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='*' element={<Missing/>}/>
-          <Route path='/footer' element={<Footer/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  const hasChild = useOutlet();
+
+  const RootContent = () => <div>App /</div>;
+
+  return <div>{hasChild || <RootContent />}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Header from "./Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import Footer from "./Footer";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "header",
+        element: <Header />,
+      },
+      {
+        path: "nav",
+        element: <Nav />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "post",
+        element: <NewPost />,
+      },
+      {
+        path: "post/:id",
+        element: <PostPage />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "footer",
+        element: <Footer />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Missing />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={App} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
